bda438043b538a25a83f8b2fc2bfaf054458a3ca
shota4214 <seshiruff4214@yahoo.co.jp>
2022-09-21 18:25:18 +0900

    :sparkles: ユーザーshow画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'top#index'
+  resources :users, only: [:show]
 end
