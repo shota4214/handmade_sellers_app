3af807cdda17e482fa9e365dba80a294f56f14e9
shota4214 <seshiruff4214@yahoo.co.jp>
2022-09-21 17:42:49 +0900

    :sparkles: 新規登録・ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :image_cache, :admin, :keep_shop_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :image_cache, :admin, :keep_shop_id])
+  end
 end
