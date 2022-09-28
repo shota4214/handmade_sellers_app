206d0a6d11682bac67643a85bf6d6b3da9bc510f
shota4214 <seshiruff4214@yahoo.co.jp>
2022-09-28 17:48:32 +0900

    sparkles: 材料登録実装

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,23 +1,33 @@
 class MaterialsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_material, only: %i[show edit update destroy]
+  before_action :set_shop, only: %i[new create]
 
   def index
     @materials = Material.all
   end
 
   def new
-    @shop = Shop.find(params[:shop_id])
     @material = Material.new
   end
 
   def create
+    @material = @shop.materials.build(material_params)
+    if @material.save
+      redirect_to top_shop_path(params[:shop_id]), notice: "材料を登録しました"
+    else
+      render :new
+    end
   end
 
   private
 
   def set_material
     @material = Material.find(params[:id])
+  end
+
+  def set_shop
+    @shop = Shop.find(params[:shop_id])
   end
 
   def material_params
