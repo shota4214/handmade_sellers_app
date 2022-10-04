597a0d01c451f62f074a00796c87b71782611cbe
shota4214 <seshiruff4214@yahoo.co.jp>
2022-10-04 18:49:34 +0900

    :sparkles: 商品作成時材料選択実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,32 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_shop, only: %i[index]
+  before_action :set_shop, only: %i[index new]
   before_action :set_product, only: %i[]
 
   def index
-    @products = Product..where(shop: @shop)
+    @products = Product.where(shop: @shop)
   end
 
   def new
     @product = Product.new
+    @product.productions.build
+    @materials = Material.where(shop: @shop)
   end
 
   def create
   end
 
+  def get_material
+    @material = Material.find(params[:material_id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :image, :image_cache, :product_cost_price, :sales_price, :cost_rate, :create_date, :product_stock, :remarks)
+    params.require(:product).permit(:name, :image, :image_cache, :product_cost_price, :sales_price, :cost_rate, :create_date, :product_stock, :remarks, production_attributes: [:id, :material_id, :product_id, :use_material_number, :use_cost_price, :_destroy, material_attributes: [:stock]])
   end
 
   def set_shop
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   mount_uploader :image, ProductImageUploader
   has_many :productions, dependent: :destroy
   has_many :materials, through: :productions
+  accepts_nested_attributes_for :productions, allow_destroy: true
   belongs_to :shop
 end
