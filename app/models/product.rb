86f64bb26e67791f60a9a00880e2bfd7e1f2873c
shota4214 <seshiruff4214@yahoo.co.jp>
2022-10-10 23:56:20 +0900

    :sparkles: 商品作成ページ材料登録時のjquery実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   has_many :materials, through: :productions
   accepts_nested_attributes_for :productions, allow_destroy: true
   belongs_to :shop
+  validates :product_stock, numericality: { greater_than_or_equal_to: 1 }
 end
