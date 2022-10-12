fa17dceed3f2d555c4ace3e3298c4ef04c0f9ad3
shota4214 <seshiruff4214@yahoo.co.jp>
2022-10-12 22:57:20 +0900

    :sparkles: 商品作成ページ材料登録実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_shop, only: %i[index new]
+  before_action :set_shop, only: %i[index new create]
   before_action :set_product, only: %i[]
 
   def index
@@ -14,6 +14,20 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = @shop.products.build(product_params)
+    use_materials = params[:product][:productions_attributes]
+    if @product.save
+      use_materials.each do |use_material|
+        material_info = use_material[1]
+        material = Material.find(material_info.fetch("material_id"))
+        number_used = material_info.fetch("use_material_number").to_i
+        material_stock = material.stock - number_used
+        material.update!(stock: material_stock)
+      end
+      redirect_to top_shop_path(@shop), notice: "商品を登録しました"
+    else
+      render :new
+    end
   end
 
   def get_material
@@ -29,7 +43,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :image, :image_cache, :product_cost_price, :sales_price, :cost_rate, :create_date, :product_stock, :remarks, production_attributes: [:id, :material_id, :product_id, :use_material_number, :use_cost_price, :_destroy, material_attributes: [:stock]])
+    params.require(:product).permit(:name, :image, :image_cache, :product_cost_price, :sales_price, :cost_rate, :create_date, :product_stock, :remarks, productions_attributes: [:id, :material_id, :product_id, :use_material_number, :use_cost_price, :_destroy])
   end
 
   def set_shop
